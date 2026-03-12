064e8e38d53cb61b3072605c98cc2d1f32389176
Mr-Smilin <Mr-Smilin@users.noreply.github.com>
2026-03-12 06:30:12 +0000

    deploy: b0ef6afa4b070d9abbbf2d444ee4ca83c2bca6d8

--- assets/app-BEKDVkkU.js
+++ assets/app-BEKDVkkU.js
@@ -1,39 +1,39 @@
-import{a as e,i as t,n,t as r}from"./app-rLFl3LGl.js";var i=n(),a=t(),o=`/react-ssg/assets/react-CHdo91hT.svg`,s=`/react-ssg/vite.svg`,c=`#root {\r
-	max-width: 1280px;\r
-}\r
-\r
-.logo {\r
-    height: 6em;\r
-    padding: 1.5em;\r
-    will-change: filter;\r
-    transition: filter 300ms;\r
-}\r
-.logo:hover {\r
-    filter: drop-shadow(0 0 2em #646cffaa);\r
-}\r
-.logo.react:hover {\r
-    filter: drop-shadow(0 0 2em #61dafbaa);\r
-}\r
-\r
-@keyframes logo-spin {\r
-    from {\r
-        transform: rotate(0deg);\r
-    }\r
-    to {\r
-        transform: rotate(360deg);\r
-    }\r
-}\r
-\r
-@media (prefers-reduced-motion: no-preference) {\r
-    a:nth-of-type(2) .logo {\r
-        animation: logo-spin infinite 20s linear;\r
-    }\r
-}\r
-\r
-.card {\r
-    padding: 2em;\r
-}\r
-\r
-.read-the-docs {\r
-    color: #888;\r
+import{a as e,i as t,n,t as r}from"./app-D0PLz4Dp.js";var i=n(),a=t(),o=`/react-ssg/assets/react-CHdo91hT.svg`,s=`/react-ssg/vite.svg`,c=`#root {
+	max-width: 1280px;
+}
+
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.react:hover {
+    filter: drop-shadow(0 0 2em #61dafbaa);
+}
+
+@keyframes logo-spin {
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
+
+@media (prefers-reduced-motion: no-preference) {
+    a:nth-of-type(2) .logo {
+        animation: logo-spin infinite 20s linear;
+    }
+}
+
+.card {
+    padding: 2em;
+}
+
+.read-the-docs {
+    color: #888;
 }`,l=e(),u=r`${c}`;function d(){let e=(0,i.c)(13),[t,n]=(0,a.useState)(0),r;e[0]===Symbol.for(`react.memo_cache_sentinel`)?(r=(0,l.jsx)(u,{}),e[0]=r):r=e[0];let c;e[1]===Symbol.for(`react.memo_cache_sentinel`)?(c=(0,l.jsx)(`a`,{href:`https://vite.dev`,target:`_blank`,children:(0,l.jsx)(`img`,{src:s,className:`logo`,alt:`Vite logo`})}),e[1]=c):c=e[1];let d,p;e[2]===Symbol.for(`react.memo_cache_sentinel`)?(d=(0,l.jsxs)(`div`,{children:[c,(0,l.jsx)(`a`,{href:`https://react.dev`,target:`_blank`,children:(0,l.jsx)(`img`,{src:o,className:`logo react`,alt:`React logo`})})]}),p=(0,l.jsx)(`h1`,{children:`Vite + React`}),e[2]=d,e[3]=p):(d=e[2],p=e[3]);let m;e[4]===Symbol.for(`react.memo_cache_sentinel`)?(m=()=>n(f),e[4]=m):m=e[4];let h;e[5]===t?h=e[6]:(h=(0,l.jsxs)(`button`,{onClick:m,children:[`count is `,t]}),e[5]=t,e[6]=h);let g;e[7]===Symbol.for(`react.memo_cache_sentinel`)?(g=(0,l.jsxs)(`p`,{children:[`Edit `,(0,l.jsx)(`code`,{children:`src/pages/app/index.jsx`}),` and save to test HMR`]}),e[7]=g):g=e[7];let _;e[8]===h?_=e[9]:(_=(0,l.jsxs)(`div`,{className:`card`,children:[h,g]}),e[8]=h,e[9]=_);let v;e[10]===Symbol.for(`react.memo_cache_sentinel`)?(v=(0,l.jsx)(`p`,{className:`read-the-docs`,children:`Click on the Vite and React logos to learn more`}),e[10]=v):v=e[10];let y;return e[11]===_?y=e[12]:(y=(0,l.jsxs)(l.Fragment,{children:[r,d,p,_,v]}),e[11]=_,e[12]=y),y}function f(e){return e+1}var p=d;export{p as default};
